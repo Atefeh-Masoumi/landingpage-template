1236285ec990cef7e85da807b8650aee3ddd1cba
Atefeh Masoumi <atefeh.masoomy@gmail.com>
2022-05-23 17:00:32 +0430

    finally done2

--- my-app2/src/App.js
+++ my-app2/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { MDBBtn, MDBContainer } from 'mdb-react-ui-kit';
+
 import Navbar from './component/Navbar';
 import Header from './component/header';
 import Body from './component/Body';
 import ProductImage from './component/productImage.js';
 import Footer from './component/Footer';
-import Newsection from './component/Newproduct';
+
 
 function App() {
   return (
@@ -14,7 +14,6 @@ function App() {
       <Header/>
       <Body/>
       <ProductImage/>
-      {/* <Newsection/> */}
       <Footer/>
       </>
   );
